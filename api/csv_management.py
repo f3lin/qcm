766b116cf8876c2ff54e690f2602627ab5b07dbb
nal95 <alex.nguepi@yahoo.fr>
2024-05-28 22:27:51 +0000

    correct typo; clean code and add doc to methodes

--- api/csv_management.py
+++ api/csv_management.py
@@ -19,7 +19,7 @@ def load_csv(url: str,  destination: str):
     df.to_csv(destination, index=False)
 
 
-def verify_data_existance(question: CSVQuestion, df: pd.DataFrame) -> bool:
+def verify_question_subject_and_subject_existence(question: CSVQuestion, df: pd.DataFrame) -> bool:
     """
     Verifies if the provided question exists in the DataFrame.
 
@@ -34,12 +34,26 @@ def verify_data_existance(question: CSVQuestion, df: pd.DataFrame) -> bool:
     if not df[(df['question'] == question.question) & (df['subject'] == question.subject)].empty:
         return True
     
+    return False
+
+
+def verify_question_correct_existence(question: CSVQuestion, df: pd.DataFrame) -> bool:
+    """
+    Verifies if the correct answer of the provided question is empty.
+
+    Args:
+        question (CSVQuestion): The question to verify.
+        df (pd.DataFrame): The DataFrame containing the questions.
+
+    Returns:
+        bool: True if the correct answer is empty, False otherwise.
+    """
     if question.correct=="":
         return True 
     
     return False
 
-def verify_use_existance(use: str, df: pd.DataFrame) -> bool:
+def verify_use_existence(use: str, df: pd.DataFrame) -> bool:
     """
     Verifies if the provided 'use' exists in the DataFrame.
 
@@ -50,23 +64,21 @@ def verify_use_existance(use: str, df: pd.DataFrame) -> bool:
     Returns:
         bool: True if the 'use' exists, False otherwise.
     """
-    return True if use in df['use'].unique() else False
+    return use in df['use'].unique()
 
-def verify_subject_existance(subjects: List[str], df: pd.DataFrame) -> bool:
+def verify_subject_existence(subjects: List[str], df: pd.DataFrame) -> bool:
     """
-    Verifies if the provided subjects exist in the DataFrame.
+    Verifies if the provided subjects exist in the 'subject' column of the DataFrame.
 
     Args:
         subjects (List[str]): The list of subjects to verify.
-        df (pd.DataFrame): The DataFrame containing the questions.
+        df (pd.DataFrame): The DataFrame containing the 'subject' column.
 
     Returns:
         bool: True if all subjects exist, False otherwise.
     """
-    for subject in subjects:
-        if subject not in df['subject'].unique():
-            return False
-    return True
+    unique_subjects = set(df['subject'].unique())
+    return all(subject in unique_subjects for subject in subjects)
 
 def convert_df_to_CSVQuestion_List(df: pd.DataFrame):
     """
@@ -78,6 +90,9 @@ def convert_df_to_CSVQuestion_List(df: pd.DataFrame):
     Returns:
         List[CSVQuestion]: A list of CSVQuestion objects.
     """
+    df=df.fillna('')
+    df['remark'] = df['remark'].fillna('')
+
     df_list = [CSVQuestion(**row) for row in df.to_dict(orient='records')]
     return df_list
 
@@ -106,18 +121,18 @@ def get_questions(use: str, subjects: List[str], num_questions: int, df: pd.Data
     Returns:
         List[CSVQuestion]: A list of CSVQuestion objects.
     """
-    if not verify_use_existance(use, df):
+    if not verify_use_existence(use, df):
         raise ValueError("The use you provide is not available. Availables Uses are: {uses}".format(uses=df['use'].unique()))
-    elif not verify_subject_existance(subjects, df):
+    if not verify_subject_existence(subjects, df):
         raise ValueError("The subjects you provide are not available. Availables Subjects are: {subjects}".format(subjects=df['subject'].unique()))
-    else:
-        filtered_questions = df[(df['use'] == use) & (df['subject'].isin(subjects))]
-        if len(filtered_questions) < num_questions:
-            raise ValueError("Not enough questions available for the specified criteria. Number of questions available: {questions}".format(questions=len(filtered_questions)))
-        
-        filtered_questions = filtered_questions.sample(n=num_questions)
-        filtered_questions_list = convert_df_to_CSVQuestion_List(filtered_questions)
-        return filtered_questions_list
+    
+    filtered_questions = df[(df['use'] == use) & (df['subject'].isin(subjects))]
+    if len(filtered_questions) < num_questions:
+        raise ValueError("Not enough questions available for the specified criteria. Number of questions available: {questions}".format(questions=len(filtered_questions)))
+    
+    filtered_questions = filtered_questions.sample(n=num_questions)
+    filtered_questions_list = convert_df_to_CSVQuestion_List(filtered_questions)
+    return filtered_questions_list
 
 def add_question(question: CSVQuestion, df: pd.DataFrame, destination: str):
     """
@@ -127,9 +142,45 @@ def add_question(question: CSVQuestion, df: pd.DataFrame, destination: str):
         question (CSVQuestion): The question to add.
         df (pd.DataFrame): The DataFrame containing the questions.
         destination (str): The path to the destination file.
+        
+    Raises:
+        ValueError: If the question already exists in the DataFrame or if the correct answer is empty.
     """
-    if not verify_data_existance(question, df):
-        # Convert the new question to a dictionary and add it to the DataFrame
-        new_row = pd.DataFrame([question.model_dump()])
-        df = pd.concat([df, new_row], ignore_index=True)
-        df.to_csv(destination, index=False)
+    if verify_question_subject_and_subject_existence(question, df):
+         raise ValueError("Question already exists")
+    
+    if verify_question_correct_existence(question, df):
+         raise ValueError("Question correct answer can't be empty")
+    
+    # Convert the new question to a dictionary and add it to the DataFrame
+    new_row = pd.DataFrame([question.model_dump()])
+    df = pd.concat([df, new_row], ignore_index=True)
+    df.to_csv(destination, index=False)
+
+# TODO: should be tested
+def remove_question(question: CSVQuestion, df: pd.DataFrame, destination: str):
+    """
+    Deletes a question from the DataFrame and saves the updated DataFrame to the destination file.
+
+    Args:
+        question (CSVQuestion): The question to delete.
+        df (pd.DataFrame): The DataFrame containing the questions.
+        destination (str): The path to the destination file.
+
+    Raises:
+        ValueError: If the question does not exist in the DataFrame.
+    """
+    if not verify_question_subject_and_subject_existence(question, df):
+        raise ValueError("Question does not exist")
+
+    # Find the index of the question to delete
+    index_to_delete = df[(df['question'] == question.question) & (df['subject'] == question.subject)].index[0]
+
+    # Delete the question from the DataFrame
+    df.drop(index_to_delete, inplace=True)
+
+    # Save the updated DataFrame to the destination file
+    df.to_csv(destination, index=False)
+    
+    return True
+
